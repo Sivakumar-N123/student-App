863e1c161a0793184d34a4423b620e936dab72e5
1sivakumar <91527131+1sivakumar@users.noreply.github.com>
2022-09-19 14:12:20 +0530

    intial commit

---
+++ Data/studentAppDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentApp.ViewModel;
+
+namespace StudentApp.Data
+{
+    public class studentAppDBContext: DbContext
+    {
+        public studentAppDBContext(DbContextOptions<studentAppDBContext>options): base(options)
+        {
+                
+        }
+        public DbSet<UserViewModel> UserTable { get; set; }
+    }
+}
---
+++ ViewModel/UserViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudentApp.ViewModel
+{
+    public class UserViewModel
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public int User { get; set; }
+        [Required]
+        public string UserName { get; set; }
+        [Required]
+        public int Password { get; set; }
+        [Required]
+        public bool IsStudent { get; set; }  
+        public bool IsActive { get; set; }
+    }
+}
