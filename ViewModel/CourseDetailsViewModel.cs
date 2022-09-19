00e9a698209a68293a341059f485a1d7eeb7ab6a
Arthiii <arthi.v@qbrainx.com>
2022-09-19 17:26:11 +0530

    data commit

---
+++ ViewModel/CourseDetailsViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace studentApp.ViewModel
+{
+    public class CourseDetailsViewModel
+    {
+        [Key]
+        public int CourseId { get; set; }
+        [Required]
+        public string CourseName { get; set; }
+       
+        
+    }
+}
---
+++ ViewModel/SpecificationViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace studentApp.ViewModel
+{
+    public class SpecificationViewModel
+    {
+        [Key]
+         public int ID { get; set; }
+        [Required]
+        public string SpecificationName { get; set; }
+
+    }
+}
