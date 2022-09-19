fa39553c308b15fd998e02b415f6f6c0884c7833
mahima m <mahima.m@qbrainx.com>
2022-09-19 16:55:00 +0530

    data added

---
+++ ViewModel/CourseDetails.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace studentApp.ViewModel
+{
+    public class CourseDetails
+    {
+        [Key]
+        public int CourseId { get; set; }
+        public string CourseName { get; set; }
+
+    }
+}
